9e2dc005a0ea7dd5d21ef71f6ab9f17ac922db1e
tilek <tagbwoi@gmail.com>
2022-12-24 23:44:00 +0600

    first

---
+++ 2day3.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function vowelsAndConsonants(s) {
+     const vowels = ["a", "e", "i", "o", "u"];
+    for (let i=0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            console.log(s[i]);
+        }
+    }
+    for (let i=0; i < s.length; i++) {
+        if (!vowels.includes(s[i])) {
+            console.log(s[i]);
+        }
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
